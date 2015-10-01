258bb3583d9c0b3dc931e5ff21b14c17b09004c2
Dmitry Lagoza <dmitry@lagoza.name>
2015-10-01 06:38:31 +0200

    + [fluentd_forwarder] Multiple hosts and ips support

--- output/fluentd_forwarder/fluentd_forwarder.go
+++ output/fluentd_forwarder/fluentd_forwarder.go
@@ -2,31 +2,54 @@ package fluentd_forwarder
 
 import (
 	basiclogger "../../basiclogger"
+	"crypto/tls"
 	"fmt"
 	"gopkg.in/vmihailenco/msgpack.v2"
 	"log"
+	"math/rand"
 	"net"
 	"os"
-	"strconv"
+	"regexp"
 	"time"
 )
 
 const module = "fluentd_forwarder"
 
 type Fluentd_forwarder struct {
-	tag     string
-	c       chan *basiclogger.Message
-	conn    net.Conn
-	host    string
-	timeout time.Duration
+	tag        string
+	c          chan *basiclogger.Message
+	conn       net.Conn
+	connTLS    tls.Conn
+	tlsEnabled bool
+	host       string
+	hosts      []string
+	timeout    time.Duration
+}
+
+var hostname string
+var hostport_re, _ = regexp.Compile("^(.+):([0-9]+)$")
+
+func init() {
+	hostname, _ = os.Hostname()
+	rand.Seed(time.Now().UnixNano())
 }
 
 func Init(conf map[string]interface{}) *Fluentd_forwarder {
-	if host, ok := conf["host"]; !ok {
-		log.Fatal("[", module, "] You must specify host")
+	if hosts_raw, ok := conf["hosts"]; !ok || len(hosts_raw.([]interface{})) == 0 {
+		log.Fatal("[", module, "] You must specify hosts")
 	} else {
-		if port, ok := conf["port"]; !ok || port.(int64) <= 0 {
-			log.Fatal("[", module, "] You must specify right port")
+		var hosts []string
+		for _, hostport_raw := range hosts_raw.([]interface{}) {
+			hostport := hostport_raw.(string)
+			submatch := hostport_re.FindSubmatch([]byte(hostport))
+			if submatch == nil {
+				log.Printf("[%s] Invalid host:port given: %s", module, hostport)
+			} else {
+				hosts = append(hosts, hostport)
+			}
+		}
+		if len(hosts) == 0 {
+			log.Fatal("[", module, "] There is no valid hosts")
 		} else {
 			if timeout, ok := conf["timeout"]; !ok || timeout.(int64) <= 0 {
 				log.Fatal("[", module, "] You must specify right timeout")
@@ -36,7 +59,7 @@ func Init(conf map[string]interface{}) *Fluentd_forwarder {
 					tag:     tag.(string),
 					c:       make(chan *basiclogger.Message),
 					conn:    nil,
-					host:    host.(string) + ":" + strconv.Itoa(int(port.(int64))),
+					hosts:   hosts,
 					timeout: time.Second * time.Duration(timeout.(int64))}
 			}
 		}
@@ -51,11 +74,10 @@ func (v Fluentd_forwarder) Tag() string {
 //TODO: SSL support
 func (v Fluentd_forwarder) Send(message *basiclogger.Message) error {
 	var err error
-	now := time.Now().Unix()
+	now := time.Now().UnixNano()
 	for {
 		if v.conn == nil {
-			if v.conn, err = net.DialTimeout("tcp", v.host, v.timeout); err != nil {
-				log.Printf("%v", err)
+			if !v.connect() {
 				if v.conn != nil {
 					v.conn.Close()
 				}
@@ -63,7 +85,7 @@ func (v Fluentd_forwarder) Send(message *basiclogger.Message) error {
 				continue
 			}
 		}
-		message.Data["host"], _ = os.Hostname()
+		message.Data["host"] = hostname
 		val := []interface{}{v.tag, now, message.Data}
 		m, err := msgpack.Marshal(val)
 		if err != nil {
@@ -81,4 +103,38 @@ func (v Fluentd_forwarder) Send(message *basiclogger.Message) error {
 		break
 	}
 	return err
+}
+
+func (v Fluentd_forwarder) connect() bool {
+	hostport := v.hosts[rand.Int()%len(v.hosts)]
+	submatch := hostport_re.FindSubmatch([]byte(hostport))
+	host := string(submatch[1])
+	port := string(submatch[2])
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Printf("[%s] DNS lookup failure \"%s\": %s", v.tag, host, err)
+		return false
+	}
+
+	ip := ips[rand.Int()%len(ips)]
+	var addressport string
+
+	address := ip.String()
+	if len(ip) == net.IPv4len {
+		addressport = fmt.Sprintf("%s:%s", address, port)
+	} else if len(ip) == net.IPv6len {
+		addressport = fmt.Sprintf("[%s]:%s", address, port)
+	}
+
+	log.Printf("[%s] Connecting to %s (%s) \n", v.tag, addressport, host)
+
+	conn, err := net.DialTimeout("tcp", addressport, v.timeout)
+	if err != nil {
+		log.Printf("[%s] Failure connecting to %s: %s\n", v.tag, address, err)
+		return false
+	} else {
+		v.conn = conn
+	}
+	return false
 }
