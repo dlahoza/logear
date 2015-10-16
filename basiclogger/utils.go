bbc7f90a2642567c264d87c7bacdfda7e6775926
Dmitry Lagoza <dmitry@lagoza.name>
2015-10-17 00:23:51 +0200

    + [filetail] formating timestamp support

--- basiclogger/utils.go
+++ basiclogger/utils.go
@@ -3,7 +3,7 @@ package basiclogger
 import "time"
 
 func ConvertTimestamp(timestamp_format, timestamp string) string {
-	tf := "2006-01-02T15:04:05.999999Z"
+	tf := "2006-01-02T15:04:05.999999"
 	t, err := time.Parse(timestamp_format, timestamp)
 	if err == nil {
 		return t.Format(tf)
